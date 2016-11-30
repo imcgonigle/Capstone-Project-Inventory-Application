c7cabf5ff447dfed665fe558f59ee423b21998b5
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-11-30 10:05:06 -0700

    run knex init and migrate:make

---
+++ knexfile.js
@@ -0,0 +1,46 @@
+module.exports = {
+
+  development: {
+    client: 'postgres',
+    connection: 'postgresql://localhost/inventory_application',
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		}
+	},
+	
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+			directory: './database/migrations'
+    },
+		seeds: {
+			directory: './database/seeds'
+		}
+  }
+
+};
