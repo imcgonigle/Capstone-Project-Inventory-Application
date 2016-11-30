a97d295dd50cc5d9e97cfca89ae0b4b599ca530a
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-11-30 10:26:16 -0700

    add user migrations

--- database/migrations/20161130100524_users.js
+++ database/migrations/20161130100524_users.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+	return knex.schema.createTable('users', function(table) {
+		table.increments('id');
+		table.string('email').notNullable().unique();
+		table.string('password_hash').notNullable();
+		table.string('first_name');
+		table.string('last_name');
+		table.text('bio');
+		table.dateTime('created_at');
+		table.dateTime('updated_at');
+	});
+
 };
 
 exports.down = function(knex, Promise) {
-  
+	return knex.schema.dropTable('users');
 };
