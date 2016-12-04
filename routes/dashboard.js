2549455da0493dafe71ac6ac4b8b4848d73dbb7c
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-12-04 13:21:40 -0700

    add routes for settings and collections

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', isLoggedIn(), function(req, res, next) {
-    res.render('/dashboard/index', {
+router.get('/', isLoggedIn, function(req, res, next) {
+    res.render('dashboard/index', {
         user: req.user,
         title: "| Dashboard"
     });
 });
+
+router.get('/collections', isLoggedIn, function(req, res, next) {
+    res.render('dashboard/collections', {
+        user: req.user,
+        title: '| Collections'
+    });
+});
+
+router.get('/settings', isLoggedIn, function(req, res, next) {
+    res.render('dashboard/settings', {
+        user: req.user,
+        title: '| Settings'
+    });
+});
+
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+};
 
 module.exports = router;
