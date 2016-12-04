a007631866cd69394f923d5c543a704483bd4970
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-12-04 12:59:25 -0700

    remove console logs

--- config/passport.js
+++ config/passport.js
@@ -1,70 +1,74 @@
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-var userQueries = require('../database/queries/user_queries');
+"user strict"
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const userQueries = require('../database/queries/user_queries');
 
 
 module.exports = function(passport) {
 
-  // used to serialize the user for the session
-  passport.serializeUser(function(user, done) {
-      done(null, user);
-  });
-
-  // used to deserialize the user
-  passport.deserializeUser(function(user, done) {
-		console.log('deserialize', user);
-		userQueries.getUserByGoogleID(user.google_id)
-		.then(function(data) {
-			done(null, data[0]);
-		})
-		.catch(function(error) {
-			done(error);
-		});
-  });
-
-
-  passport.use(new GoogleStrategy({
-      clientID        : process.env.GOOGLE_CLIENT_ID,
-      clientSecret    : process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL     : process.env.GOOGLE_CALLBACK_URL,
-  },
-  function(token, refreshToken, profile, done) {
-    process.nextTick(function() {
-
-			userQueries.getUserByGoogleID(profile.id)
-			.then(function(users) {
-				let user = users[0];
-
-				if(user && user.google_id == profile.id) {
-					console.log("Found a user")
-					done(null, user);
-				} else {
-					console.log('Didn\'t find a user time to make one');
-					let newUser = {};
-					newUser.googleID = profile.id;
-					newUser.email = profile.emails[0].value;
-					newUser.firstName = profile.name.givenName;
-					newUser.lastName = profile.name.familyName;
-					newUser.photoURL = profile.photos[0].value;
-
-					userQueries.addUser(newUser)
-					.then(function() {
-						let user = {};
-						user.google_id = profile.id;
-						user.newUser = true;
-						done(null, user);
-					})
-					.catch(function(err) {
-						console.log('Error adding user');
-						done(err);
-					})
-				}
-			})
-			.catch(function(err) {
-				console.log('Error finding user');
-				done(err);
-			})
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        done(null, user);
     });
 
-  }));
+    // used to deserialize the user
+    passport.deserializeUser(function(user, done) {
 
+        userQueries.getUserByGoogleID(user.google_id)
+            .then(function(data) {
+                done(null, data[0]);
+            })
+            .catch(function(error) {
+                done(error);
+            });
+    });
+
+
+    passport.use(new GoogleStrategy({
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        },
+        function(token, refreshToken, profile, done) {
+            process.nextTick(function() {
+
+                userQueries.getUserByGoogleID(profile.id)
+                    .then(function(users) {
+                        let user = users[0];
+
+                        if (user && user.google_id == profile.id) {
+                            done(null, user);
+                        } else {
+
+                            let newUser = {
+                                googleID: profile.id,
+                                email: profile.emails[0].value,
+                                firstName: profile.name.givenName,
+                                lastName: profile.name.familyName,
+                                photoURL: profile.photos[0].value,
+                            };
+
+
+                            userQueries.addUser(newUser)
+                                .then(function() {
+
+                                    let user = {
+                                        google_id: profile.id,
+                                        newUser: true
+                                    };
+
+                                    done(null, user);
+                                })
+                                .catch(function(err) {
+                                    done(err, null);
+                                })
+                        }
+                    })
+                    .catch(function(err) {
+                        done(err, null);
+                    })
+            });
+
+        }));
+				
 };
