420a2999083e87faae943a1242d0e2a6e499ceb8
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-12-04 15:01:40 -0700

    add description and created/update times

--- database/migrations/20161201195116_collections.js
+++ database/migrations/20161201195116_collections.js
@@ -1,13 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('collections', function(table) {
-		table.increments('id');
-		table.integer('owner_id');
-		table.integer('item_count');
-		table.string('name');
-	});
+    return knex.schema.createTable('collections', function(table) {
+        table.increments('id');
+        table.integer('owner_id');
+        table.integer('item_count');
+        table.string('name');
+        table.text('description');
+        table.date('created_at');
+        table.date('updated_at');
+    });
 };
 
 exports.down = function(knex, Promise) {
-	return knex.schema.dropTable('collections');
+    return knex.schema.dropTable('collections');
 };
