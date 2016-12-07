7997781f661450eea7e8b50e1f9de694f7c6ed1f
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-12-06 19:56:23 -0700

    added images and styles

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var passport = require('passport')
+const express = require('express');
+const router = express.Router();
+const passport = require('passport')
 
 // route for home page
 router.get('/', (req, res, next) => {
