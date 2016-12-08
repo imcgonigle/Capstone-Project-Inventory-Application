66ce2c8ac6f2561322935bac7d97fe8651174c6c
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-12-08 10:47:40 -0700

    fix spelling error

--- database/migrations/20161204213753_items.js
+++ database/migrations/20161204213753_items.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('items', function(table) {
         table.increments('id');
         table.integer('collection_id')
-            .refernces('id')
+            .references('id')
             .inTable('collections')
             .onDelete('CASCADE');
         table.string('name').notNullable().unique();
